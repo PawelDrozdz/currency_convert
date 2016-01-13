50adb6991aa2997db453de1ee29be558a2f1d68d
Pawel Drozdz <drozdz.pj@gmail.com>
2016-01-13 18:31:13 +0100

    style changes

--- Pawel_Drozdz_task.php
+++ Pawel_Drozdz_task.php
@@ -10,10 +10,10 @@
         <div id="converter">
             <h1 class="title">Currency Converter</h1>
             <form>
-                RUB: <input type="text" id="varRUB"><br>
+                RUB amount: <input type="text" id="varRUB"><br>
 			<button type="button" onclick="convertCurrency('varRUB')">Convert to PLN</button>
             </form>
-            <h3 id="convertedCurrency">Type the amount to convert and click "Convert to PLN"</p>
+            <h2 id="convertedCurrency">Type the amount to convert and click "Convert to PLN"</p>
         </div>
     </div>
 </body>
